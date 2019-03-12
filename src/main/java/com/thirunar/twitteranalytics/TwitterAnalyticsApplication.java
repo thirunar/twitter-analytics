13246781b684bd3650c1af132023861314d074e5
Thirunavukkarasu Ravichandran <thirunavukkarasu.ravichandran@webtrekk.com>
2019-03-12 20:11:32 +0100

    initial commit

---
+++ src/main/java/com/thirunar/twitteranalytics/TwitterAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.thirunar.twitteranalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thirunar/twitteranalytics/TwitterAnalyticsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thirunar.twitteranalytics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwitterAnalyticsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
